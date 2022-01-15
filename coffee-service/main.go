a73a9d9a2fd043ac086cd4afc76c7fc01c1c290c
val antonini <val.antonini@protonmail.com>
2022-01-15 12:29:20 +1000

    hello world json api

--- coffee-service/main.go
+++ coffee-service/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := make(map[string]string)
+		data["hello"] = "world"
+
+		res, err := json.Marshal(data)
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			http.Error(w, "500 internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
