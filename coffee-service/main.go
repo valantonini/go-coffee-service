212b856ae5481fa0298dbfba33536a2508d22266
val antonini <val.antonini@protonmail.com>
2022-01-22 06:15:11 +1000

    missed files from service extraction

--- coffee-service/main.go
+++ coffee-service/main.go
@@ -6,8 +6,6 @@ import (
 	"log"
 	"net/http"
 	"valantonini/go-coffee-service/coffee-service/config"
-	"valantonini/go-coffee-service/coffee-service/data"
-	"valantonini/go-coffee-service/coffee-service/events"
 	"valantonini/go-coffee-service/coffee-service/service"
 )
 
@@ -16,7 +14,6 @@ func main() {
 	if err != nil {
 		panic("unable to create configuration")
 	}
-	repository, _ := data.InitRepository()
 
 	cfg.Logger.Printf("connecting to nats on %v\n", cfg.NatsAddress)
 	nc, err := nats.Connect(cfg.NatsAddress)
@@ -25,39 +22,16 @@ func main() {
 	}
 	cfg.Logger.Println("connected to nats")
 
-	coffeeService := service.NewCoffeeService(cfg)
+	cfg.Logger.Println("registering coffee services")
+	coffeeService := service.NewCoffeeService(cfg, nc)
 	http.HandleFunc("/list", coffeeService.List)
-	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			printRoutes(w, r)
-			return
-		}
-
-		var requestData map[string]string
-		decoder := json.NewDecoder(r.Body)
-		decoder.Decode(&requestData)
-		newItem := repository.Add(requestData["name"])
-
-		res, err := json.Marshal(newItem)
-		if err != nil {
-			log.Printf("Error during JSON marshal. Err: %s", err)
-			http.Error(w, "500 internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		nc.Publish(events.CoffeeAdded, res)
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(res)
-	})
-
+	http.HandleFunc("/add", coffeeService.Add)
 	http.Handle("/health", service.NewHealth(cfg.Logger))
+	cfg.Logger.Println("coffee services registered")
 
 	cfg.Logger.Printf("starting server on %v", cfg.BindAddress)
-
 	if err := http.ListenAndServe(cfg.BindAddress, nil); err != nil {
-		log.Fatal(err)
+		cfg.Logger.Fatal(err)
 	}
 }
 func printRoutes(w http.ResponseWriter, r *http.Request) {
--- coffee-service/service/service.go
+++ coffee-service/service/service.go
@@ -1,25 +1,30 @@
 package service
 
 import (
+	"encoding/json"
+	"github.com/nats-io/nats.go"
 	"log"
 	"net/http"
 	"valantonini/go-coffee-service/coffee-service/config"
 	"valantonini/go-coffee-service/coffee-service/data"
+	"valantonini/go-coffee-service/coffee-service/events"
 )
 
 // A coffee service handler
 type CoffeeService interface {
 	List(w http.ResponseWriter, r *http.Request)
+	Add(w http.ResponseWriter, r *http.Request)
 }
 
 type coffeeService struct {
 	repository data.Repository
+	nats       nats.Conn
 	logger     log.Logger
 }
 
 // NewCoffeeService creates a new instance of the coffee service
-func NewCoffeeService(c *config.Config) CoffeeService {
-	return &coffeeService{*c.Repository, *c.Logger}
+func NewCoffeeService(c *config.Config, nc *nats.Conn) CoffeeService {
+	return &coffeeService{*c.Repository, *nc, *c.Logger}
 }
 
 func (c *coffeeService) List(w http.ResponseWriter, r *http.Request) {
@@ -27,10 +32,30 @@ func (c *coffeeService) List(w http.ResponseWriter, r *http.Request) {
 
 	res, err := data.ToJSON()
 	if err != nil {
-		log.Printf("Error during JSON marshal. Err: %s", err)
+		c.logger.Printf("Error during JSON marshal. Err: %s", err)
 		http.Error(w, "500 internal server error", http.StatusInternalServerError)
 		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func (c *coffeeService) Add(w http.ResponseWriter, r *http.Request) {
+	var requestData map[string]string
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestData)
+	newItem := c.repository.Add(requestData["name"])
+
+	res, err := json.Marshal(newItem)
+	if err != nil {
+		c.logger.Printf("Error during JSON marshal. Err: %s", err)
+		http.Error(w, "500 internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	c.nats.Publish(events.CoffeeAdded, res)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
